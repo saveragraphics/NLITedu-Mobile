e12174c60e658d59e3b8339d38e1c5096968730c
masterrahulind-tech <masterrahul.ind@gmail.com>
2026-05-16 16:52:46 +0530

    Enhanced Live Class WebView with WebRTC support and permissions

--- lib/features/course/live_class_screen.dart
+++ lib/features/course/live_class_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lucide_icons/lucide_icons.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import '../../models/live_session.dart';
 import '../../providers/live_provider.dart';
 import '../../providers/learning_service.dart';
@@ -25,11 +27,30 @@ class _LiveClassScreenState extends ConsumerState<LiveClassScreen> {
   void initState() {
     super.initState();
     
-    // 1. Log Attendance & Start Duration Timer (Option A)
+    // 1. Log Attendance & Start Duration Timer
     _startAttendanceLogging();
 
-    // 2. Initialize WebView
-    _controller = WebViewController()
+    // 2. Initialize WebView with permissions
+    _initWebView();
+  }
+
+  Future<void> _initWebView() async {
+    // Request Camera and Microphone permissions for WebRTC
+    await [Permission.camera, Permission.microphone].request();
+
+    final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserActionForPlayback: const <PlaybackMediaType>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    _controller = WebViewController.fromPlatformCreationParams(params);
+
+    _controller
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
@@ -41,15 +62,23 @@ class _LiveClassScreenState extends ConsumerState<LiveClassScreen> {
             setState(() => _isLoading = false);
           },
         ),
-      )
-      ..loadRequest(Uri.parse(widget.session.sessionUrl));
+      );
+
+    // Specific Android Permission handling
+    if (_controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (_controller.platform as AndroidWebViewController)
+          .setOnPermissionRequest((request) => request.grant());
+    }
+
+    _controller.loadRequest(Uri.parse(widget.session.sessionUrl));
   }
 
   Future<void> _startAttendanceLogging() async {
     // Basic attendance log (existing)
     ref.read(liveServiceProvider).logAttendance(widget.session);
     
-    // Advanced duration log for "Weekly Goal" (Option A)
+    // Advanced duration log for "Weekly Goal"
     try {
       final logId = await ref.read(learningServiceProvider).startSessionSession(widget.session.id);
       setState(() => _attendanceLogId = logId);
@@ -60,7 +89,7 @@ class _LiveClassScreenState extends ConsumerState<LiveClassScreen> {
 
   @override
   void dispose() {
-    // 3. End Duration Timer & Calculate Weekly Goal Hours on exit
+    // 3. End Duration Timer on exit
     if (_attendanceLogId != null) {
       ref.read(learningServiceProvider).endSessionSession(_attendanceLogId!);
     }
